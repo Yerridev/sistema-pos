457d75ce169a7a1f0d7faaa62b3f9af75ba9bd99
Pietro-Puluche-uss <pespejopietrora@uss.edu.pe>
2026-05-20 20:06:24 -0500

    Agregar dashboard de ventas y caja

--- config/urls.py
+++ config/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
 
     # Dashboard
     path('', include('productos.urls')),
+    path('', include('ventas.dashboard_urls')),
 
     # Auth endpoints (API)
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
